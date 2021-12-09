eb3094e7c01522059fb99bd1e3ef553b6223e21c
Metin Çelebi <metincelebi5534@hotmail.com>
2021-12-09 14:21:56 +0300

    Add project files.

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,91 @@
+﻿using API.Core.DbModels;
+using API.Core.Interfaces;
+using API.Core.Specifications;
+using API.Dtos;
+using API.Helpers;
+using API.Infrastructure.DataContext;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    // https:localhost:5001/api/products/GetProduct
+
+    public class ProductsController : BaseApiController
+    {
+        //private readonly StoreContext _context;
+        //private readonly IProductRepository _productRepository;
+
+        private readonly IGenericRepository<Product> _productRepository;
+        private readonly IGenericRepository<ProductBrand> _productBrandRepository;
+        private readonly IGenericRepository<ProductType> _productTypeRepository;
+
+        private readonly IMapper _mapper;
+
+        public ProductsController(IGenericRepository<Product> productRepository,
+            IGenericRepository<ProductBrand> productBrandRepository,
+             IGenericRepository<ProductType> productTypeRepository, IMapper mapper)
+        {
+            _productRepository = productRepository;
+            _productBrandRepository = productBrandRepository;
+            _productTypeRepository = productTypeRepository;
+            _mapper = mapper;
+            //_context = context;
+        }
+
+
+        [HttpGet] //http verbs -> get post delete put 
+        public async Task<ActionResult<Pagination<ProductToReturnDto>>> GetProducts([FromQuery]ProductSpecParams productSpecParams)
+        {
+            var spec = new ProductsWithProductTypeAndBrandsSpecification(productSpecParams);
+
+            var countSpec = new ProductWithFiltersForCountSpecification(productSpecParams);
+
+            var totalItems = await _productRepository.CountAsync(countSpec);
+
+            var products = await _productRepository.ListAsync(spec);
+
+            var data = _mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products);
+
+            return Ok(new Pagination<ProductToReturnDto>(productSpecParams.PageIndex, productSpecParams.PageSize, totalItems, data));
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id)
+        {
+            var spec = new ProductsWithProductTypeAndBrandsSpecification(id);
+
+            // return await _productRepository.GetEntityWithSpec(spec);
+
+            var product = await _productRepository.GetEntityWithSpec(spec);
+            //return new ProductToReturnDto
+            //{
+            //    Id = product.Id,
+            //    Name = product.Name,
+            //    Description = product.Description,
+            //    PictureUrl = product.PictureUrl,
+            //    Price = product.Price,
+            //    ProductBrand = product.ProductBrand != null ? product.ProductBrand.Name : string.Empty,
+            //    ProductType = product.ProductType != null ? product.ProductType.Name : string.Empty
+            //};
+            return _mapper.Map<Product, ProductToReturnDto>(product);
+        }
+
+        [HttpGet("brands")]
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
+        {
+            return Ok(await _productBrandRepository.ListAllAsync());
+        }
+
+        [HttpGet("types")]
+        public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
+        {
+            return Ok(await _productTypeRepository.ListAllAsync());
+        }
+    }
+}
